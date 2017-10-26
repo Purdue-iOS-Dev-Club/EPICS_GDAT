3eb0c39259bccd92fada8fea19e340115a650078
Sruthi Pillai <sruthipillai@pal-nat186-67-205.itap.purdue.edu>
2017-10-26 17:37:12 -0400

    Local Storage View Controller added

---
+++ EPICS_GDAT/LocalStorageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LocalStorageViewController.swift
+//  EPICS_GDAT
+//
+//  Created by Sruthi Pillai on 10/26/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class LocalStorageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
