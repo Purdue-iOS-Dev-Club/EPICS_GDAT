40a0048e016fb5b2d3ee8c4676f38a700d850386
Siraj Zaneer <szaneer@gmail.com>
2017-10-26 17:37:04 -0400

    Bluetooth View Controller added

---
+++ EPICS_GDAT/BluetoothViewController.swift
@@ -0,0 +1,35 @@
+\//
+//  BluetoothViewController.swift
+//  EPICS_GDAT
+//
+//  Created by Siraj Zaneer on 10/26/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class BluetoothViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
