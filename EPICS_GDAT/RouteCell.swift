22aa3dd1c00c29ef05acce332474849b7dff0b1a
Siraj Zaneer <szaneer@gmail.com>
2017-11-30 15:25:25 -0500

    Added collectionview for routes and refactored speech to use prompts

---
+++ EPICS_GDAT/RouteCell.swift
@@ -0,0 +1,13 @@
+//
+//  RouteCell.swift
+//  EPICS_GDAT
+//
+//  Created by Siraj Zaneer on 11/30/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class RouteCell: UICollectionViewCell {
+    
+}
